8ea4ad8c54f31cc7961374f1fc96a310685e719e
Mehmet <mralkanlar@gmail.com>
2021-01-20 17:18:23 -0800

    Use etherscan api

--- record_volume.py
+++ record_volume.py
@@ -4,6 +4,7 @@ import decimal
 import json
 import logging
 import os
+import requests
 
 import web3
 
@@ -20,6 +21,7 @@ FLEEK_KEY_ID = os.environ["FLEEK_KEY_ID"]
 FLEEK_KEY = os.environ["FLEEK_KEY"]
 FLEEK_BUCKET = os.environ["FLEEK_BUCKET"]
 
+ETHERSCAN_ENDPOINT = "https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp={}&closest=before"
 SWAP_CONTRACT_ABI_PATH = "Swap.json"
 # Recent block number + a buffer
 ETH_BLOCKS_IN_A_DAY = 6533 + 1000
@@ -38,6 +40,19 @@ TOKEN_DECIMALS = [
 event_block_cache = {}
 
 
+def get_day_ago_block_number():
+    now = datetime.datetime.now()
+    day_ago = datetime.timedelta(days=1)
+    cutoff_dt = now - day_ago
+    cutoff_ts = round(cutoff_dt.timestamp())
+    try:
+        response = requests.get(ETHERSCAN_ENDPOINT.format(cutoff_ts)).json()
+        return int(response["result"])
+
+    except Exception as e:
+        logger.error(e)
+
+
 def main(args):
     try:
         f = open(SWAP_CONTRACT_ABI_PATH)
@@ -47,17 +62,11 @@ def main(args):
         logger.error(f"Could not load swap contract ABI: {e}")
 
     w3 = web3.Web3(web3.Web3.HTTPProvider(HTTP_PROVIDER_URL))
-    current_block = w3.eth.blockNumber
 
-    starting_block = current_block - ETH_BLOCKS_IN_A_DAY
-    now = datetime.datetime.now()
-    day_ago = datetime.timedelta(days=1)
-    cutoff_dt = now - day_ago
-    cutoff_ts = cutoff_dt.timestamp()
+    day_ago_block_number = get_day_ago_block_number()
+
     contract = w3.eth.contract(abi=swap_contract_abi, address=SWAP_CONTRACT_ADDRESS)
-    swaps = contract.events.TokenSwap.createFilter(
-        fromBlock=starting_block, toBlock=current_block
-    )
+    swaps = contract.events.TokenSwap.createFilter(fromBlock=day_ago_block_number)
 
     fleek_aws_client = get_fleek_client(FLEEK_KEY_ID, FLEEK_KEY)
 
@@ -67,23 +76,14 @@ def main(args):
         len_events = len(all_events)
 
         logger.info(f"Fetched {len_events} events.")
-        counted = 0
         for event in all_events:
-
-            event_block = event.blockNumber
-            if event_block not in event_block_cache:
-                block_data = w3.eth.getBlock(event_block)
-                event_block_cache[event_block] = block_data
-            if event_block_cache[event_block].timestamp < cutoff_ts:
-                continue
-            counted += 1
             decimals = TOKEN_DECIMALS[event.args.soldId]
             token_amount = decimal.Decimal(event.args.tokensSold) / (
                 decimal.Decimal(10) ** decimals
             )
             total_tokens_sold += token_amount
 
-        payload = {"totalTokens": float(total_tokens_sold), "swaps": counted}
+        payload = {"totalTokens": float(total_tokens_sold), "swaps": len_events}
         logger.info(f"Uploading payload: {payload}")
         payload_bytes = json.dumps(payload, separators=(",", ":")).encode("utf-8")
         fleek_aws_client.put_object(
