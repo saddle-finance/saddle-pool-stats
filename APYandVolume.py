533dcd934067bacce1ca6521cb086d361898e0cc
Bobby Battista <bobtista@gmail.com>
2021-05-11 15:52:01 -0600

    split out get prices and get volume functions

--- APYandVolume.py
+++ APYandVolume.py
@@ -9,73 +9,77 @@ class Swap:
         self.name = name
         self.tokenList = tokens
 
-stableSwapCoins = ["dai", "usd-coin", "tether"]
-btcSwapCoins = ["tbtc", "wrapped-bitcoin", "renbtc", "sbtc"]
-vETHSwapCoins = ["weth", "veth2"]
-
+# pool addresses (We call them swaps):
 stableSwapAddress = "0x3911f80530595fbd01ab1516ab61255d75aeb066"
 btcSwapAddress = "0x4f6a43ad7cba042606decaca730d4ce0a57ac62e"
 vETH2SwapAddress = "0xdec2157831d6abc3ec328291119cc91b337272b5"
 
+def getTokenPricesUSD(tokenPricesUSD):
+    CGTokenNames = ["saddlebtc", "saddleusd", "saddleveth2", "dai", "usd-coin", "tether",  "tbtc", "wrapped-bitcoin", "renbtc", "sbtc", "weth"]
 
-swaps = {
-    stableSwapAddress: Swap("Stable", stableSwapCoins),
-    btcSwapAddress: Swap("Btc", btcSwapCoins),
-    vETH2SwapAddress: Swap("vETH2", vETHSwapCoins)
-}
+    r = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd".format(','.join(CGTokenNames)))
 
-graphURL = "https://api.thegraph.com/subgraphs/name/saddle-finance/saddle"
+    for token, price in r.json().items():
+        tokenPricesUSD[token] = price["usd"]
 
-tokenPricesUSD = dict()
-
-CGTokenNames = ["saddlebtc", "saddleusd", "saddleveth2", "dai", "usd-coin", "tether",  "tbtc", "wrapped-bitcoin", "renbtc", "sbtc", "weth"]
-
-r = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=usd".format(','.join(CGTokenNames)))
-
-for token, price in r.json().items():
-    tokenPricesUSD[token] = price["usd"]
-
-yesterday = int(time.time()) - 3600*24
-swapsGraphQuery = """{{
-  swaps {{
-    address
-    exchanges(first: 1) {{
-      boughtId
-      tokensBought
-      soldId
-      tokensSold
+def get24HrVolume(tokenPricesUSD, swaps):
+    graphURL = "https://api.thegraph.com/subgraphs/name/saddle-finance/saddle"
+    yesterday = int(time.time()) - 3600*24
+    swapsGraphQuery = """{{
+    swaps {{
+        address
+        exchanges(first: 1) {{
+        boughtId
+        tokensBought
+        soldId
+        tokensSold
+        }}
     }}
-  }}
-}}""".format(yesterday)
+    }}""".format(yesterday)
 
-r = requests.post(graphURL, data=json.dumps({"query": swapsGraphQuery}))
+    r = requests.post(graphURL, data=json.dumps({"query": swapsGraphQuery}))
 
-for swap in r.json()["data"]["swaps"]:
-    currentPool = swaps[swap["address"]]
-    for exchange in swap["exchanges"]:
-        boughtToken = currentPool.tokenList[int(exchange["boughtId"])]
-        tokenAmount = 0
-        tokenPrice = 0
-        # If we're in the VETH pool we don't have a price for VETH2, so make sure to use WETH 
-        if swap["address"] == vETH2SwapAddress:
-            # if the token bought was wETH we can use the price and amount
-            tokenPrice = tokenPricesUSD[currentPool.tokenList[0]]
-            if exchange["boughtId"] == "0":
-                tokenAmount = exchange["tokensBought"] # this number has no decimal, need to math
+    for swap in r.json()["data"]["swaps"]:
+        currentPool = swaps[swap["address"]]
+        for exchange in swap["exchanges"]:
+            boughtToken = currentPool.tokenList[int(exchange["boughtId"])]
+            tokenAmount = 0
+            tokenPrice = 0
+            # If we're in the VETH pool we don't have a price for VETH2, so make sure to use WETH 
+            if swap["address"] == vETH2SwapAddress:
+                # if the token bought was wETH we can use the price and amount
+                tokenPrice = tokenPricesUSD[currentPool.tokenList[0]]
+                if exchange["boughtId"] == "0":
+                    tokenAmount = exchange["tokensBought"] # this number has no decimal, need to math
+                else:
+                    tokenAmount = exchange["tokensSold"] # this number has no decimal, need to math
             else:
-                tokenAmount = exchange["tokensSold"] # this number has no decimal, need to math
-        else:
-            tokenPrice = tokenPricesUSD[boughtToken]
-            tokenAmount = int(exchange["tokensBought"])
-        
-        currentPool.volume += float(tokenPrice) * int(tokenAmount)
+                tokenPrice = tokenPricesUSD[boughtToken]
+                tokenAmount = int(exchange["tokensBought"])
+            
+            currentPool.volume += float(tokenPrice) * int(tokenAmount)
 
-# Todo write to json file instead of logging
-for address, swap in swaps.items():
-    print("pool: {} volume: {}".format(swap.name, swap.volume))
+def main():
+    stableSwapCoins = ["dai", "usd-coin", "tether"]
+    btcSwapCoins = ["tbtc", "wrapped-bitcoin", "renbtc", "sbtc"]
+    vETHSwapCoins = ["weth", "veth2"]
 
+    swaps = {
+        stableSwapAddress: Swap("Stable", stableSwapCoins),
+        btcSwapAddress: Swap("Btc", btcSwapCoins),
+        vETH2SwapAddress: Swap("vETH2", vETHSwapCoins)
+    }
 
+    tokenPricesUSD = dict()
+    getTokenPricesUSD(tokenPricesUSD)
+    get24HrVolume(tokenPricesUSD, swaps)
 
+    # Todo write to json file instead of logging
+    for address, swap in swaps.items():
+        print("pool: {} volume: {}".format(swap.name, swap.volume))
+    
+if __name__ == "__main__":
+    main()
             
 
 
