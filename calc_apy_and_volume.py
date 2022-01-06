2eac3a30996b4ebf4c638d5558761011d753a059
David Hamme <dhamme@gmail.com>
2022-01-06 10:43:36 -0800

    Stop hardcoding token indexes (#17)
    * use address instead of id
    
    * make everything better

--- calc_apy_and_volume.py
+++ calc_apy_and_volume.py
@@ -6,6 +6,8 @@ import os
 import sys
 from dotenv import load_dotenv
 from util import get_fleek_client
+from multicall import Call, Multicall
+from web3 import Web3
 
 load_dotenv()
 
@@ -13,6 +15,7 @@ SWAP_STATS_FILE_PATH = os.environ["SWAP_STATS_FILE_PATH"]
 FLEEK_KEY_ID = os.environ["FLEEK_KEY_ID"]
 FLEEK_KEY = os.environ["FLEEK_KEY"]
 FLEEK_BUCKET = os.environ["FLEEK_BUCKET"]
+HTTP_PROVIDER_URL = os.environ["HTTP_PROVIDER_URL"]
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
@@ -22,12 +25,8 @@ btcSwapAddress = "0x4f6a43ad7cba042606decaca730d4ce0a57ac62e"
 vETH2SwapAddress = "0xdec2157831d6abc3ec328291119cc91b337272b5"
 
 # coingecko api accepts case insensitive but returns lowercase addresses
-VETH2TokenAddress = "0x898BAD2774EB97cF6b94605677F43b41871410B1".lower()
 WETHTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".lower()
 WBTCTokenAddress = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599".lower()
-TBTCv2TokenAddress = "0x18084fba666a33d37592fa2633fd49a74dd93a88".lower()
-USDv2LPTokenAddress = "0x5f86558387293b6009d7896A61fcc86C17808D62".lower()
-BTCv2LPTokenAddress = "0xF32E91464ca18fc156aB97a697D6f8ae66Cd21a3".lower()
 
 payload = {}
 EMPTY_PAYLOAD_ITEM = {
@@ -36,8 +35,40 @@ EMPTY_PAYLOAD_ITEM = {
     "TVL": 0,
 }
 
+def get_pool_token_positions(swaps):
+    w3 = Web3(Web3.HTTPProvider(HTTP_PROVIDER_URL))
+    identity = lambda x: x
+    calls = []
+    # get token positions for each pool
+    for pool in swaps:
+        for token in pool['tokens']:
+            calls.append(
+                Call(
+                    pool['address'], 
+                    ['getTokenIndex(address)(uint8)', token['address']], 
+                    [[f"{pool['address']}_{token['address']}", identity]]
+                )
+            )
+    try:
+        # execute the call
+        multi = Multicall(calls, _w3=w3)
+        response = multi()
 
-def get_token_prices_usd(tokenAddresses):
+        # parse the response
+        poolTokenPositions = dict()
+        for pool in swaps:
+            idxs = [None] * len(pool['tokens'])
+            for i, token in enumerate(pool['tokens']):
+                idxs[i] = response[f"{pool['address']}_{token['address']}"]
+            poolTokenPositions[pool['address']] = idxs
+        return poolTokenPositions
+    except Exception as e:
+        logger.error(f"Error getting pool token positions: {e}")
+        return
+
+
+def get_token_prices_usd(tokens):
+    tokenAddresses = tokens.keys()
     tokenPricesUSD = dict()
 
     try:
@@ -48,27 +79,25 @@ def get_token_prices_usd(tokenAddresses):
     except Exception as e:
         logger.error(f"Error getting price data from coinGecko: {e}")
         return
-
-    for tokenAddress, price in r.json().items():
+    response_dict = r.json()
+    for tokenAddress, price in response_dict.items():
         if price == {}:
             logger.error("Error getting price data for token: {}".format(
-                tokenAddress
+                tokens[tokenAddress]
             ))
         else:
             tokenPricesUSD[tokenAddress] = float(price["usd"])
-    # hardcode 1 USD as the price for USDv2 LP tokens
-    tokenPricesUSD[USDv2LPTokenAddress] = 1
-    # hardcode 1 WBTC as the price for BTCv2 LP tokens
-    tokenPricesUSD[BTCv2LPTokenAddress] = tokenPricesUSD[WBTCTokenAddress]
-    # hardcode 1 WBTC as the price for TBTCv2
-    # TODO: remove once coingecko has the correct price
-    tokenPricesUSD[TBTCv2TokenAddress] = tokenPricesUSD[WBTCTokenAddress]
-    # use WETH price for VETH2
-    if WETHTokenAddress in tokenPricesUSD:
-        tokenPricesUSD[VETH2TokenAddress] = tokenPricesUSD[WETHTokenAddress]
-        return tokenPricesUSD
-    else:
-        return
+    for tokenAddress, name in tokens.items():
+        if tokenAddress not in tokenPricesUSD:
+            if "usd" in name.lower():
+                tokenPricesUSD[tokenAddress] = 1
+            elif "btc" in name.lower():
+                tokenPricesUSD[tokenAddress] = tokenPricesUSD[WBTCTokenAddress]
+            elif "eth" in name.lower():
+                tokenPricesUSD[tokenAddress] = tokenPricesUSD[WETHTokenAddress]
+            else:
+                tokenPricesUSD[tokenAddress] = 1
+    return tokenPricesUSD
 
 
 def get_graph_data():
@@ -76,18 +105,19 @@ def get_graph_data():
     yesterday = int(time.time()) - 3600*24
     swapsDailyVolumeGraphQuery = """{{
         swaps {{
-            id
+            address
             balances
             tokens {{
-              id
+              address
               name
               decimals
             }}
-            exchanges(where: {{timestamp_gte: {}}}) {{
+            exchanges(where: {{timestamp_gte: {}}}, orderBy: tokensBought, orderDirection: desc) {{
                 boughtId
                 tokensBought
                 soldId
                 tokensSold
+                transaction
             }}
         }}
         }}""".format(yesterday)
@@ -101,26 +131,18 @@ def get_graph_data():
         return
 
 
-btcGraphToChainTokenIdx = [1, 0, 2, 3]
-ethGraphToChainTokenIdx = [1, 0]
-
-
-def get_one_day_volume(tokenPricesUSD, swaps):
+def get_one_day_volume(tokenPricesUSD, swaps, poolTokenPositions):
     for swap in swaps:
-        print("Processing {}".format(swap))
+        print("\nProcessing {} volume".format(swap["address"]))
         for exchange in swap["exchanges"]:
-            tokenIdx = int(exchange["boughtId"])
-            if swap["id"] == btcSwapAddress:
-                tokenIdx = btcGraphToChainTokenIdx[tokenIdx]
-            elif swap["id"] == vETH2SwapAddress:
-                tokenIdx = ethGraphToChainTokenIdx[tokenIdx]
-            print("Swap token idx: {}".format(tokenIdx))
             try:
-                boughtTokenAddress = swap["tokens"][tokenIdx]["id"]
+                tokenIdx = int(exchange["boughtId"])
+                tokenIdx = poolTokenPositions[swap["address"]][tokenIdx]
+                boughtTokenAddress = swap["tokens"][tokenIdx]["address"]
             # for metapools fallback to using the first token to calculate price
             except IndexError:
                 tokenIdx = 0
-                boughtTokenAddress = swap["tokens"][tokenIdx]["id"]
+                boughtTokenAddress = swap["tokens"][tokenIdx]["address"]
             if boughtTokenAddress in tokenPricesUSD:
                 boughtTokenPrice = tokenPricesUSD[boughtTokenAddress]
             else:
@@ -130,24 +152,24 @@ def get_one_day_volume(tokenPricesUSD, swaps):
 
             boughtTokenAmount = exchange["tokensBought"]
             decimals = int(swap["tokens"][tokenIdx]["decimals"])
-            payload.setdefault(swap["id"], dict(EMPTY_PAYLOAD_ITEM))
-            payload[swap["id"]]["oneDayVolume"] += (
+            payload.setdefault(swap["address"], dict(EMPTY_PAYLOAD_ITEM))
+            parsed_amount = (
                 float(boughtTokenPrice) * int(boughtTokenAmount)
             ) / (10 ** decimals)
+            payload[swap["address"]]["oneDayVolume"] += parsed_amount
+            txn = exchange["transaction"]
+            print(f"{txn} swapped ${int(parsed_amount):>12,d}")
 
 
-def get_swap_tvls(tokenPricesUSD, swaps):
+def get_swap_tvls(tokenPricesUSD, swaps, poolTokenPositions):
     for swap in swaps:
         for idx, token in enumerate(swap["tokens"]):
-            priceUSD = tokenPricesUSD[token["id"]]
+            priceUSD = tokenPricesUSD[token["address"]]
             decimals = int(token["decimals"])
-            if swap["id"] == btcSwapAddress:
-                idx = btcGraphToChainTokenIdx[idx]
-            elif swap["id"] == vETH2SwapAddress:
-                idx = ethGraphToChainTokenIdx[idx]
-            balance = int(swap["balances"][idx])
-            payload.setdefault(swap["id"], dict(EMPTY_PAYLOAD_ITEM))
-            payload[swap["id"]]["TVL"] += (balance * priceUSD) / (10**decimals)
+            tokenIdx = poolTokenPositions[swap["address"]][idx]
+            balance = int(swap["balances"][tokenIdx])
+            payload.setdefault(swap["address"], dict(EMPTY_PAYLOAD_ITEM))
+            payload[swap["address"]]["TVL"] += (balance * priceUSD) / (10**decimals)
 
 
 def calculate_apys():
@@ -175,26 +197,35 @@ def write_to_ipfs():
 
 
 def get_token_addresses(swaps):
-    tokenAddresses = list()
+    map = dict()
     for swap in swaps:
         for token in swap["tokens"]:
-            tokenAddresses.append(token["id"].lower())
-    return tokenAddresses
+            map[token["address"].lower()] = token["name"]
+    return map
 
 
 def main():
     swapsData = get_graph_data()
     if swapsData is None:
         return
+    pool_token_positions = get_pool_token_positions(swapsData)
     tokenAddresses = get_token_addresses(swapsData)
-    print("Using token addresses: {}".format(tokenAddresses))
+    print("\n/********** Token Addresses **********/")
+    print("\n".join(list(map(lambda pair: f"{pair[1]:<25} {pair[0]}", tokenAddresses.items()))))
+
     tokenPricesUSD = get_token_prices_usd(tokenAddresses)
-    print("Using token prices: {}".format(tokenPricesUSD))
+    print("\n/********** Token Prices **********/")
+    print("\n".join(list(map(lambda pair: f"{tokenAddresses[pair[0]]:<25} {pair[1]:>10,.3f}", sorted(tokenPricesUSD.items(), key=lambda pair: pair[1])))))
     if tokenPricesUSD is None:
         return
-    get_one_day_volume(tokenPricesUSD, swapsData)
-    get_swap_tvls(tokenPricesUSD, swapsData)
+    print("\n/********** Exchanges **********/")
+    get_one_day_volume(tokenPricesUSD, swapsData, pool_token_positions)
+    get_swap_tvls(tokenPricesUSD, swapsData, pool_token_positions)    
     calculate_apys()
+    print("\n/********** Final Result **********/")
+    print(f"{'Address':<50} {'APY':<10} {'Volume':<10} {'TVL':<13}")
+    for address, data in sorted(payload.items(), key=lambda pair: pair[1]["TVL"], reverse=True):
+        print(f"{address:<50} {data['APY']:<10.4f} {int(data['oneDayVolume']):>10,d} {int(data['TVL']):>13,d}")
     write_to_ipfs()
 
 
